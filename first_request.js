820990683aa500e07c8f31a80b2d6e1f2ef4dad1
Obi <obi.netbits@outlook.com>
2016-05-28 17:53:45 +0000

    add Hawii weather api

--- first_request.js
+++ first_request.js
@@ -7,15 +7,12 @@ request('http://www.google.com', function (error, response, body) {
   }
 });*/
 
+console.log("Sunset in Hawii is at...")
 var request = require('request');
-request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function(error, response, body){
-    if(error){
-        console.log("SOMETHING WENT WRONG");
-        console.log(error);
-    } else {
-        if(response.statusCode == 200){
-            console.log(body);
-        }
-    }
+request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function(error, response, body){
+    if(!error && response.statusCode == 200){
+        var parsedData = JSON.parse(body);
+        console.log(parsedData["query"]["results"]["channel"]["astronomy"]["sunset"]);
+    } 
 });
 
